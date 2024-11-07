c5019e5afb8fd8538663586ff0beff02a08b1181
rheakarnawat <rheakarnawat24@gmail.com>
2024-11-07 08:23:14 +0530

    7th nov

---
+++ Lab 1.py
@@ -0,0 +1,27 @@
+
+#keyword -> print
+
+print("hello riya")
+
+# underscore is a valid variable name
+# _ = 85
+# = -> assignment operator
+# variable value -> 85
+
+_ = 45
+_ = _ +1
+print(_)
+#
+#valiad variable name
+abc123 = 90
+_riya = 12
+
+#invalid variable name
+#123abc = 90
+
+riya_k = 23
+print(riya_k)
+
+print(max(3,4))
+
+
